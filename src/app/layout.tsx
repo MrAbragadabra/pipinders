937dbbe146fa0bc7f344d9b31f1e80b256d206c4
Alexander Minin <77795206+Abragadabra@users.noreply.github.com>
2024-08-06 23:37:38 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='ru'>
-			<body className={cn(nunito.className, 'bg-gradient-to-tr from-slate-900 to-pink-500 min-h-screen overflow-x-hidden')}>
+			<body className={cn(nunito.className, 'bg-gradient-to-tr from-lime-500 to-lime-700 min-h-screen overflow-x-hidden')}>
         <Header/>
 				<main className='lg:px-20 px-5 mt-16 mb-16 relative'>{children}</main>
 			</body>
