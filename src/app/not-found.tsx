f5de701fff01583a754684de3cb1e53ff0b64d42
Alexander <minin.saschencka@yandex.ru>
2024-08-06 18:03:01 +0500

    add custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+export default function NotFound() {
+	return (
+		<section>
+			<div>
+				<h2 className='text-white uppercase text-4xl font-bold text-center'>
+					Вы ошиблись адресом
+				</h2>
+				<div className='flex items-center justify-center mt-5'>
+					<Image
+						src='https://http.cat/404.jpg'
+						width={300}
+						height={300}
+						alt='404 cat'
+					/>
+				</div>
+			</div>
+		</section>
+	)
+}
