5922c2e93298c665220f07c979f9324b38a2f46a
Abragadabra <minin.saschencka@yandex.ru>
2024-08-20 21:11:53 +0500

    how to play is component

--- src/app/how-to-play/page.tsx
+++ src/app/how-to-play/page.tsx
@@ -1,21 +1,23 @@
-import { JetBrains_Mono } from 'next/font/google'
+import { HowToPlay } from '@/components/shared'
+import { Metadata } from 'next'
 
-const jetBrains_mono = JetBrains_Mono({
-	weight: ['400', '700'],
-	subsets: ['cyrillic'],
-})
+export const metadata: Metadata = {
+	title: 'Как играть? • PIPINDERS',
+	openGraph: {
+		title: 'Как играть? • PIPINDERS',
+		description:
+			'Наш сервер объединяет ванильное выживание и взаимодействие игроков, предлагая уютное и дружелюбное комьюнити, где вы сможете провести приятный вечер за игрой.',
+		url: 'https://pipinders.ru',
+		images: [
+			{
+				url: 'https://pipinders.vercel.app/og.png',
+				width: 1200,
+				height: 630,
+			},
+		],
+	},
+}
 
 export default function HowToPlayPage() {
-	return (
-		<section>
-			<div>
-				<h2 className='text-white uppercase text-4xl font-bold text-center'>
-					Как играть?
-				</h2>
-			</div>
-			<div>
-				<ul className='lg:px-20 px-10 list-decimal text-white font-semibold mt-5 text-3xl'></ul>
-			</div>
-		</section>
-	)
+	return <HowToPlay />
 }
--- src/components/shared/howToPlay.tsx
+++ src/components/shared/howToPlay.tsx
@@ -3,5 +3,16 @@ interface Props {
 }
 
 export const HowToPlay: React.FC<Props> = ({ className }) => {
-	return <h1></h1>
+	return (
+		<section>
+			<div>
+				<h2 className='text-white uppercase text-4xl font-bold text-center'>
+					Как играть?
+				</h2>
+			</div>
+			<div>
+				<ul className='lg:px-20 px-10 list-decimal text-white font-semibold mt-5 text-3xl'></ul>
+			</div>
+		</section>
+	)
 }
