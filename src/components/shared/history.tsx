7367e6fa872c2be747c8c2f4c6bb2918b6acbed2
Abragadabra <minin.saschencka@yandex.ru>
2024-08-20 21:05:25 +0500

    fucking bruh moment

---
+++ src/components/shared/history.tsx
@@ -0,0 +1,7 @@
+interface Props {
+	className?: string
+}
+
+export const History: React.FC<Props> = ({ className }) => {
+	return <h1></h1>
+}
---
+++ src/components/shared/rules.tsx
@@ -0,0 +1,7 @@
+interface Props {
+	className?: string
+}
+
+export const Rules: React.FC<Props> = ({ className }) => {
+	return <h1></h1>
+}
