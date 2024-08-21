e4d20efb8fb5c0168c4b70a545a4a1f1b62ea4ae
Daniil <baldindaniilza@mail.ru>
2024-08-21 14:07:16 +0500

    Wonderful test animations

---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+import { About } from "@/components/shared";
+
+export default function AboutPage() {
+	return <About />;
+}
---
+++ src/components/shared/about.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+interface Props {
+    
+}
+
+export const About: React.FC<Props> = () => {
+    
+    return (
+        <section className="section">
+            <h1 className="title">About Us</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in libero vel felis scelerisque consectetur. Nullam vel velit vel felis bibendum fermentum. Nullam dignissim ipsum non velit consectetur, id congue nunc mollis. Proin condimentum, ligula vel ultricies malesuada, mauris est viverra arcu, in varius ligula ligula vel ipsum.</p>
+        </section>
+    )
+}
