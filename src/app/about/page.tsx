0e513d6d7c96319aaf145598180c00b72e9099d3
Alexander <minin.saschencka@yandex.ru>
2024-08-23 21:45:54 +0500

    исправление ошибки в about и новая ссылка

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,13 +4,13 @@ import { Metadata } from 'next'
 export const metadata: Metadata = {
 	title: 'О нас • PIPINDERS',
 	openGraph: {
-		title: 'Команды сервера • PIPINDERS',
+		title: 'О нас • PIPINDERS',
 		description:
 			'Наш сервер объединяет ванильное выживание и взаимодействие игроков, предлагая уютное и дружелюбное комьюнити, где вы сможете провести приятный вечер за игрой.',
 		url: 'https://pipinders.ru',
 		images: [
 			{
-				url: 'https://pipinders.vercel.app/og.png',
+				url: 'https://pipinders.ru/og.png',
 				width: 1200,
 				height: 630,
 			},
