147086660068897952923a9ae77cd99d32cc9ecd
Abragadabra <minin.saschencka@yandex.ru>
2024-08-25 11:56:12 +0500

    change sitemap priority

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -12,37 +12,37 @@ export default function sitemap(): MetadataRoute.Sitemap {
 			url: 'https://pipinders.ru/how-to-play',
 			lastModified: new Date(),
 			changeFrequency: 'weekly',
-			priority: 0.0,
+			priority: 0.5,
 		},
 		{
 			url: 'https://pipinders.ru/rules',
 			lastModified: new Date(),
 			changeFrequency: 'weekly',
-			priority: 0.0,
+			priority: 0.5,
 		},
 		{
 			url: 'https://pipinders.ru/commands',
 			lastModified: new Date(),
 			changeFrequency: 'weekly',
-			priority: 0.0,
+			priority: 0.5,
 		},
 		{
 			url: 'https://pipinders.ru/history',
 			lastModified: new Date(),
 			changeFrequency: 'weekly',
-			priority: 0.0,
+			priority: 0.5,
 		},
 		{
 			url: 'https://pipinders.ru/donate',
 			lastModified: new Date(),
 			changeFrequency: 'weekly',
-			priority: 0.0,
+			priority: 0.5,
 		},
 		{
 			url: 'https://pipinders.ru/about',
 			lastModified: new Date(),
 			changeFrequency: 'weekly',
-			priority: 0.0,
+			priority: 0.5,
 		},
 	]
 }
