4cd52e2d79007e6574d41f04eb167b9f9fd034dd
Alexander <minin.saschencka@yandex.ru>
2024-08-30 12:07:33 +0500

    теперь кнопочки на главной при узком экране вверху

--- src/components/shared/home.tsx
+++ src/components/shared/home.tsx
@@ -25,67 +25,80 @@ export const Home: React.FC<Props> = ({ className }) => {
 					Pipinders Server
 				</h1>
 			</div>
+			<div className='flex sm:flex-col flex-col-reverse items-center'>
+				<div className='order-1'>
+					<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+						Наш сервер - уникальный проект, который совмещает в себе ванильное
+						выживание, а также взаимодействие между игроками. После того как вы
+						попадаете на наш сервер, вы окунаетесь в ламповое и дружелюбное
+						комьюнити игроков, которые всегда смогут провести с вами приятный
+						вечер за игрой.
+					</p>
+				</div>
+
+				<div className='order-2 w-full'>
+					<div className='flex items-center justify-center mt-5'>
+						<Button
+							onClick={() => handleCopy('season1.pipinders.ru')}
+							type='submit'
+							className='font-bold text-3xl p-7 bg-teal-700 hover:bg-teal-800 gap-2 sm:w-[355px] w-full'
+						>
+							Адрес 1 сезона
+							<ClipboardCopy color='#ffffff' strokeWidth={2.5} size={28} />
+						</Button>
+					</div>
+					<div className='flex items-center justify-center mt-5'>
+						<form
+							action='https://forms.yandex.ru/u/66cec1f443f74f2ba636df9a/'
+							target='_blank'
+							className='flex items-center justify-center w-full'
+						>
+							<Button
+								type='submit'
+								className='font-bold text-3xl p-7 bg-teal-700 hover:bg-teal-800 gap-2 sm:w-[355px] w-full'
+							>
+								Заявка на 2 сезон
+								<ArrowUpRight color='#ffffff' strokeWidth={2.5} size={28} />
+							</Button>
+						</form>
+					</div>
+
+					<div className='flex items-center justify-center mt-5'>
+						<form
+							action='https://t.me/pipinders_news'
+							target='_blank'
+							className='flex items-center justify-center w-full'
+						>
+							<Button
+								type='submit'
+								className='font-bold text-3xl p-7 bg-teal-700 hover:bg-teal-800 gap-2 sm:w-[355px] w-full'
+							>
+								Наш Telegram
+								<ArrowUpRight color='#ffffff' strokeWidth={2.5} size={28} />
+							</Button>
+						</form>
+					</div>
+				</div>
+			</div>
 			<div>
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
-					Наш сервер - уникальный проект, который совмещает в себе ванильное
-					выживание, а также взаимодействие между игроками. После того как вы
-					попадаете на наш сервер, вы окунаетесь в ламповое и дружелюбное
-					комьюнити игроков, которые всегда смогут провести с вами приятный
-					вечер за игрой.
-				</p>
-				<div className='flex items-center justify-center mt-5'>
-					<Button
-					onClick={() => handleCopy('season1.pipinders.ru')}
-						type='submit'
-						className='font-bold text-3xl p-7 bg-teal-700 hover:bg-teal-800 gap-2 w-[355px]'
-					>
-						Адрес 1 сезона
-						<ClipboardCopy color='#ffffff' strokeWidth={2.5} size={28} />
-					</Button>
-				</div>
-				<div className='flex items-center justify-center mt-5'>
-					<form
-						action='https://forms.yandex.ru/u/66cec1f443f74f2ba636df9a/'
-						target='_blank'
-					>
-						<Button
-							type='submit'
-							className='font-bold text-3xl p-7 bg-teal-700 hover:bg-teal-800 gap-2 w-[355px]'
-						>
-							Заявка на 2 сезон
-							<ArrowUpRight color='#ffffff' strokeWidth={2.5} size={28} />
-						</Button>
-					</form>
-				</div>
-				
-				<div className='flex items-center justify-center mt-5'>
-					<form
-						action='https://t.me/pipinders_news'
-						target='_blank'
-					>
-						<Button
-							type='submit'
-							className='font-bold text-3xl p-7 bg-teal-700 hover:bg-teal-800 gap-2 w-[355px]'
-						>
-							Наш Telegram
-							<ArrowUpRight color='#ffffff' strokeWidth={2.5} size={28} />
-						</Button>
-					</form>
-				</div>
 				<p className='text-center text-white font-semibold mt-5 text-3xl leading-10 '>
-					<span className='font-black'>28.08.2024</span> - начало приёма заявок на 2 сезон
+					<span className='font-black'>28.08.2024</span> - начало приёма заявок
+					на 2 сезон
 				</p>
 				<p className='text-center text-white font-semibold mt-5 text-3xl leading-10 '>
-					<span className='font-black'>28.08.2024</span> - открытие 1 сезона для прощания
+					<span className='font-black'>28.08.2024</span> - открытие 1 сезона для
+					прощания
 				</p>
 				<p className='text-center text-white font-semibold mt-5 text-3xl leading-10'>
-					<span className='font-black'>02.09.2024</span> - официальное закрытие 1 сезона
+					<span className='font-black'>02.09.2024</span> - официальное закрытие
+					1 сезона
 				</p>
 				<p className='text-center text-white font-semibold mt-5 text-3xl leading-10'>
 					<span className='font-black'>03.09.2024</span> - открытие 2 сезона
 					(версия 1.21.1)
 				</p>
 			</div>
+
 			{copied && (
 				<div
 					className={`max-w-[80%] w-max fixed top-20 left-1/2 -translate-x-1/2 bg-teal-950 text-white py-2 px-4 rounded text-xl z-50 std-toast ${
