f93f5caa418924ae366d4c13f72923092d2c5a95
Abragadabra <minin.saschencka@yandex.ru>
2024-09-12 00:30:31 +0500

    убрана часть с voice chat, так как он был упразднён

--- src/components/shared/howToPlay.tsx
+++ src/components/shared/howToPlay.tsx
@@ -5,7 +5,6 @@ import how2 from '../../../public/img/how/how2.png'
 import how3 from '../../../public/img/how/how3.png'
 import how4 from '../../../public/img/how/how4.png'
 import how5 from '../../../public/img/how/how5.png'
-import how6 from '../../../public/img/how/how6.png'
 
 interface Props {
 	className?: string
@@ -21,12 +20,10 @@ export const HowToPlay: React.FC<Props> = ({ className }) => {
 			</div>
 			<div>
 				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
-					Для игры на нашем сервере нужно правильно настроить клиент Minecraft.
-					В частности, следует установить мод Simple Voice Chat, без которого
-					игра на сервере будет невозможна. Также рекомендуется использовать
-					Legacy Launcher, так как на нём реализована система скинов, которая
-					позволит вам на ходу менять скин во время игры. Ниже приведена
-					инструкция по его установке и настройке.
+					Для игры на нашем сервере майнкрафт мы рекомендуем использовать
+					LegacyLauncher, так как в нём есть встроенная система скинов.
+					Благодаря этой системе + плагина на сервере можно менять скины на
+					ходу.
 				</p>
 				<ul className='lg:px-20 px-10 list-decimal text-white font-semibold mt-5 text-3xl'>
 					<li className='mb-5 text-left leading-10'>
@@ -36,7 +33,7 @@ export const HowToPlay: React.FC<Props> = ({ className }) => {
 							href='https://disk.yandex.ru/d/FsnXNe_eWWSGWw'
 							target='_blank'
 						>
-							Legacy Launcher и Simple Voice Chat (вместе с зависимостями).
+							Legacy Launcher.
 						</Link>
 					</li>
 					<li className='mb-5 text-left leading-10'>
@@ -149,11 +146,13 @@ export const HowToPlay: React.FC<Props> = ({ className }) => {
 							/>
 						</figure>
 					</li>
-					<li className='mb-5 text-left leading-10'>
+					<li className='mb-5 text-left leading-[55px]'>
 						Далее мы установим майнкрафт версии{' '}
 						<span className='highlighting'>1.21.1</span> (на такой версии
-						сервер) и установим моды. В главном меню в списке версии выбирете{' '}
-						<span className='highlighting'>Fabric 1.21.1</span>:
+						сервер). В главном меню в списке версии выбирете{' '}
+						<span className='highlighting'>Fabric 1.21.1</span> (благодаря
+						fabric вы сможете ставить моды для себя (оптимизация, мини карта и
+						тд)):
 						<figure className='flex-col items-center my-5'>
 							<Image
 								className='m-auto'
@@ -170,37 +169,6 @@ export const HowToPlay: React.FC<Props> = ({ className }) => {
 								draggable='false'
 							/>
 						</figure>
-					</li>
-					<li className='mb-5 text-left leading-10'>
-						Нажмите кнопку <span className='highlighting'>установить.</span>
-					</li>
-					<li className='mb-5 text-left leading-loose'>
-						Если после установки у вас открылся майнкрафт, то можете его закрыть
-						(если он открылся после установки), чтобы установить моды. После
-						того как вы закрыли майнкрафт, на главно нажмите на значок папки и
-						затем{' '}
-						<span className='highlighting'>Открыть папку Fabric-1.21:</span>
-						<figure className='flex-col items-center my-5'>
-							<Image
-								className='m-auto'
-								src={how6}
-								alt='скриншот'
-								height={how6.height}
-								width={how6.width}
-								sizes='100vw'
-								style={{
-									width: '100%',
-									height: 'auto',
-								}}
-								quality={100}
-								draggable='false'
-							/>
-						</figure>
-						У вас откроется проводник с папками майнкрафт. Среди этих папок
-						найдите папку <span className='highlighting'>mods</span> и
-						скопируйте в неё ранее скачанные файлы{' '}
-						<span className='highlighting'>fabric-api.jar</span> и{' '}
-						<span className='highlighting'>voicechat.jar</span>.
 					</li>
 					<li className='mb-5 text-left leading-loose'>
 						На этом и завершена настройка Legacy Launcher для игры в майнкрафт.
