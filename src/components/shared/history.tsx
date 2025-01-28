446275c1b44614b6fb9f43f943ccfa4afadb2806
MrAbragadabra <minin.saschencka@yandex.ru>
2025-01-28 11:53:09 +0500

    just text

--- src/components/shared/history.tsx
+++ src/components/shared/history.tsx
@@ -33,14 +33,14 @@ export const History: React.FC<Props> = ({ className }) => {
 				</h1>
 			</div>
 			<div>
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					Изначально у нас была идея объединения людей в одно маленькое, но
 					уютное комьюнити. Для этих целей был арендован хостинг, который
 					позволил подключаться к игре в удобное для каждого человека время. Мы
 					собрали небольшое количество людей и открыли для себя новый и
 					неизведанный мир этой игры.
 				</p>
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					С самого начала начался поиск приятного места для основания домов,
 					идея заключалась в том, что все должны быть рядом и контактировать
 					друг с другом. После поисков были выбраны небольшие острова,
@@ -49,7 +49,7 @@ export const History: React.FC<Props> = ({ className }) => {
 
 				<ImageWithCaption src={h1} caption='MIKKUT и столб грязи' />
 
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					После чего пошёл процесс облагораживания окружающей среды, была
 					освобождена большая территория и были построены дома. В процессе
 					постройки домов к нам подключались новые игроки, которые тоже начинали
@@ -59,7 +59,7 @@ export const History: React.FC<Props> = ({ className }) => {
 
 				<ImageWithCaption src={h2} caption='Дома с высоты птичьего полёта' />
 
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					А теперь поподробнее о каждой территории игроков:
 				</p>
 
@@ -68,7 +68,7 @@ export const History: React.FC<Props> = ({ className }) => {
 					caption='Территория игроков MIKKUT и Armoryx'
 				/>
 
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					Эту территорию с лёгкостью можно было увидеть благодаря высокой и
 					кривой крыше игрока MIKKUT. Когда ты зайдешь на ту территорию, твои
 					фпс упадут в ноль, скажем спасибо фермам Armoryx.
@@ -79,7 +79,7 @@ export const History: React.FC<Props> = ({ className }) => {
 					caption='Территория игроков Konstantin_mogg и Nurkaev'
 				/>
 
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					От такого количества коров все пк хотели взлетать в стратосферу, а вид
 					на этот дворец… Сразу видно богатых людей, опытных фермеров. (Коровы в
 					итоге все сбежали, что повлекло за собой нашествие коров на сервере).
@@ -87,7 +87,7 @@ export const History: React.FC<Props> = ({ className }) => {
 
 				<ImageWithCaption src={h5} caption='Территория игрока MrAbragadabra' />
 
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					На этой территории была начата стройка дома, которая шла до конца
 					сезона, а также эта территория подверглась МАССОВОЙ вырубке деревьев
 					(тут был сто проц лес).
@@ -95,23 +95,23 @@ export const History: React.FC<Props> = ({ className }) => {
 
 				<ImageWithCaption src={h6} caption='Территория игрока Steiden' />
 
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					Изначально было самой высокой башней на сервере. Этот огонь виден даже
 					если ты в аду.
 				</p>
 
 				<ImageWithCaption src={h7} caption='Территория игрока SharkPop' />
 
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					На этой территории SharkPop построил дом, но так и не использовал его
 					по назначению, но зато с радостью принял надпись SVO перед своим
 					выходом!).
 				</p>
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					После того как мы рассмотрели дома игроков, естесно нужно узнать
 					игроков поподробнее:
 				</p>
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					MrAbragadabra – Игрок, который был основателем этой территории, именно
 					благодаря ему мы видим дома именно здесь. Приложил руку к основым
 					событиям на сервере. Ну и самое главное это МОДЕРАТОР, так что его
@@ -119,39 +119,39 @@ export const History: React.FC<Props> = ({ className }) => {
 					устройство сервера, его правила и законы. Сервер избавил человека от
 					дипрашн и подарил надежду.
 				</p>
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					MIKKUT – Игрок, который строил такие невероятные сооружения как: фигня
 					с большой крышей (дом), а также строил хайперлуп в нижнем мире
 					(кривой). Сделал сортировку фермы мобов (сломал сортировку). Сервер
 					также избавил человека от дипрашн и подарил надежду.
 				</p>
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					Armoryx – Игрок, который майн видел лет 100 назад, когда динозавры
 					котельникову жрали. Быстро погрузился в игру, познал множество новых
 					механик. Построил ферму, которая ломала пк всем игрокам.
 				</p>
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					SharkPop – Игрок, который прославился своим подходом к игре, он играет
 					в игру как будто у нас спидран. Начал создание долгостроя, но так и не
 					достроил. Смешно помирает.
 				</p>
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					Steiden – Эта легенда настолько много времени проводил в пещерах с
 					самого старта сервера, что решил, что солнце это лишняя штука.
 					Построил большую башню.
 				</p>
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					Nurkaev – Вместе с Konstantin_mogg застроил большую территорию, и
 					развёл настолько большое количество животных, что лаги на их
 					территории преследовали до самого конца сезона.
 				</p>
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					Konstantin_mogg – Во время игры на сервере ощутил себя
 					землевладельцем, поэтому отстроил большой дворец, а самое главное
 					засадил картофанчиком всё и коровами. Ходили слухи, что у него
 					существовала секретная дача, которую не могли найти.
 				</p>
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					После знакомства с персонажами первого сезона, перейдём к событиям
 					сезона, а также интересным скриншотам:
 				</p>
@@ -202,7 +202,7 @@ export const History: React.FC<Props> = ({ className }) => {
 
 				<ImageWithCaption src={h19} caption='Игорь. Просто Игорь…' />
 
-				<p className='text-justify text-white font-semibold mt-5 text-3xl leading-10'>
+				<p className='just-text'>
 					После прочитанного вы можете понять, что наш сервер окружен приятными
 					людьми, которые могут смешно шутить (надеемся), а также помогут вам в
 					игре. Заходите на сервер и погрузитесь в удивительные истории, которые
